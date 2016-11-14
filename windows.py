07181e83abb4b43917f7cb6894af04adc41d0ec4
Miron Jakubowski <mijakubowski@gmail.com>
2016-11-14 20:20:46 +0100

    open db file dialog - in progress

--- windows.py
+++ windows.py
@@ -1,9 +1,12 @@
-from PyQt5.QtWidgets import QWidget, QDesktopWidget, QMessageBox, QMainWindow, QAction
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QWidget, QDesktopWidget, QMessageBox, QMainWindow, QAction, QDialog,
+    QGridLayout, QLabel, QLineEdit, QPushButton)
 from PyQt5.QtGui import QIcon
+from time import sleep
 
-class BaseW(QWidget):
-    def __init__(self):
-        super(BaseW, self).__init__()
+class BaseW(QDialog):
+    def __init__(self, parent=None):
+        super(BaseW, self).__init__(parent)
 
     def _init(self, title, w, h, posX=0, posY=0):
         self.resize(w,h)
@@ -23,6 +26,7 @@ class MainW(QMainWindow):
     def __init__(self, app):
         super(MainW, self).__init__()  
         self._init(app, "PyRecords", 800, 500)
+        self._wasShown = False
 
     def _init(self, app, title, w, h, posX=0, posY=0):        
         self._app = app
@@ -66,17 +70,46 @@ class MainW(QMainWindow):
         return menuAction
 
     def _openDbFile(self):
-        pass
+        opendbw = OpenDbW(self)
+        r = opendbw.exec()
+        print(r)
 
-#class SplashW(BaseW):
-#    def __init__(self):
-#        super(SplashW, self).__init__()
-#        self._init("Welcome...", 300, 200)
-#
-#    def closeEvent(self, event):
-#        answer = QMessageBox.question(self,"Exiting...", "Are you sure?",
-#        QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
-#
-#        if answer == QMessageBox.No:
-#            event.ignore()
-    
+    def showEvent(self, event):
+        if not self._wasShown:
+            self._wasShown = True
+            self._openDbFile()
+
+
+class OpenDbW(BaseW):
+    def __init__(self, mainW):
+        super(OpenDbW, self).__init__(mainW)
+        self._init("Open db file...", 450, 200)
+        self._createWidgets()
+        self._createLayout()
+
+    def _createWidgets(self):
+        self._dbFile = QLineEdit()
+
+        self._dbPass = QLineEdit()
+        self._dbPass.setEchoMode(QLineEdit.Password) 
+        
+        self._openFileBtn = QPushButton("...")
+
+        #TODO: add buttons to dialog within QHBoxLayoyt within QVBoxLayout
+        #self._okBtn = QPushButton("OK")
+        #self._cancelBtn = QPushButton("Cancel")
+
+    def _createLayout(self):
+        #TODO: add grid within QVBoxLayout
+        grid = QGridLayout()
+        #grid.setSpacing(10)
+
+        grid.addWidget(QLabel("Database file"), 0, 0)
+        grid.addWidget(self._dbFile, 0, 1)
+        grid.addWidget(self._openFileBtn, 0, 2)
+
+        grid.addWidget(QLabel("Password"), 1, 0)
+        grid.addWidget(self._dbPass, 1, 1)        
+
+        self.setLayout(grid)
+        
